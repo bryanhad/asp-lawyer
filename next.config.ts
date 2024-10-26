ece41d48a3299ca9c093add58e13c3f87ee8a6b0
Bryan Hadinata <bryanhadinata76@gmail.com>
2024-10-26 17:41:24 +0700

    add prisma and opt in to routing cache

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
-import type { NextConfig } from "next";
-import createNextIntlPlugin from 'next-intl/plugin';
+import type { NextConfig } from 'next'
+import createNextIntlPlugin from 'next-intl/plugin'
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin()
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    /* config options here */
+    experimental: {
+        staleTimes: {
+            dynamic: 30,
+        },
+    },
+}
 
-export default withNextIntl(nextConfig);
+export default withNextIntl(nextConfig)
